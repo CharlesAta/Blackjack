17d3454aeec117c381375b191dbf5fef542f8e52
Charles Ata <charlesata@hotmail.ca>
2021-07-21 17:54:23 -0400

    Update styling. Update app.js functions: startingWallet, handleBet, addBet, updateBet, clearBet, updateWallet, lockBetWindow, unlockBetWindow, updateEarnings, disableBets, enableBets, disableBet1000, disableBet500, disableBet100, setBetToZero, setEarningsToZero, updateWinnings, disablePlay, enablePlay, disableDouble, enableDouble

--- blackjack/js/app.js
+++ blackjack/js/app.js
@@ -16,6 +16,7 @@ const TWENTY_ONE = 21;
 const HAND_DIV_MIN_LIMIT = 3;
 const HAND_DIV_MAX_LIMIT = 4;
 
+const player = new Audio();
 const sounds = {
     playSound: './css/sounds/playSound.mp3',
     refreshSound: './css/sounds/refreshSound.mp3',
@@ -26,19 +27,34 @@ const sounds = {
     tieSound: './css/sounds/tieSound.mp3'
 }
 
+const STARTING_MONEY = 1000;
+const ONE_HUNDRED = 100;
+const FIVE_HUNDRED = 500;
+const ONE_THOUSAND = 1000;
+
 /*----- app's state (variables) -----*/
 let playersHand = [];
 let dealersHand = [];
-let wltScore = [0, 0, 0];
-let outputMessage = '';
 let playersHandTotal = 0;
 let dealersHandTotal = 0;
-let winner = '';
-let hit = false;
 let cardsInPlay = [];
+
+let wltScore = [0, 0, 0];
+let winner = '';
+
+let outputMessage = '';
+
+let hit = false;
 let startClickCount = false;
 let resetScore = false;
 let flip = false;
+let inPlay = false;
+let madeBet = false;
+let resetBet = false;
+
+let wallet = 0;
+let bet = 0;
+let earnings = 0;
 
 /*----- cached element references -----*/
 const playBtn = document.querySelector('#play-btn');
@@ -59,15 +75,31 @@ const playerArea = document.querySelector('#player');
 const currentHandArea = document.querySelector('#current-hand > span');
 const dealerCurrentHandArea = document.querySelector('#dealer-current-hand > span');
 
-const player = new Audio();
 const bgPlayer = document.querySelector('#bg-player');
+
+const bettingArea = document.querySelector('#betting-area');
+const walletAmount = document.querySelector('#wallet-amount > p');
+const betAmount = document.querySelector('#bet-amount > p');
+const bet100 = document.querySelector('#bet-100');
+const bet500 = document.querySelector('#bet-500');
+const bet1000 = document.querySelector('#bet-1000');
+const betResetButton = document.querySelector('#bet-reset');
+const earningsAmount = document.querySelector('#earnings-amount > p');
+const doubleButton = document.querySelector('#double-btn');
 
 /*----- event listeners -----*/
 options.addEventListener('click', handleClick);
 resetScoreBtn.addEventListener('click', resetScoreboard);
+bettingArea.addEventListener('click', handleBet);
+
+/*----- code ------*/
+bgPlayer.volume = .02;
+player.volume = .03;
+
+onLoad();
+createDeck();
 
 /*----- functions -----*/
-
 function createDeck(){
     // Function to create an array of objects for all 52 cards in the deck
         for(let i = 0; i < suits.length; i++) {
@@ -92,6 +124,7 @@ function onLoad() {
     // Function to be called on the intial loading of the JS to the page
     disableMoves();
     changeMesageLights();
+    startingWallet();
 }
 
 function handleClick(evt) {
@@ -110,17 +143,26 @@ function handleClick(evt) {
 
 function init() {
     // Function to initialize starting values
+    inPlay = true;
+    winner = '';
+    cardsInPlay = [];
+    flip = false;
+    startClick = true;
+    lockBetWindow();
+    disableBets();
     changeMesageLights();
     resetWinnerGlow();
+    setEarningsToZero();
+    updateEarnings();
 
-    flip = false;
-    
-    startClick = true;
+    if (inPlay && bet > 0){
+        disablePlay();
+    }
+
     if (startClick){
         playButtonText('reset');
     }
-    winner = '';
-    cardsInPlay = [];
+    
     playSound('dealSound');
     startingCards(playerArea, playersHand, playersHandTotal, 'player');
     startingCards(dealerArea, dealersHand, dealersHandTotal, 'dealer');
@@ -211,7 +253,6 @@ function checkWinner() {
         winner = 'tie';
         playerArea.style.alignContent = 'center';
         playSound('tieSound');
-        addWinnerGlow(winner);
     }
     // Define when player wins
     else if (playersHandTotal === TWENTY_ONE || 
@@ -220,7 +261,6 @@ function checkWinner() {
             winner = 'player';
             playerArea.style.alignContent = 'center';
             playSound('winSound');
-            addWinnerGlow(winner);
     }
     // Define when dealer wins
     else if (dealersHandTotal === TWENTY_ONE ||
@@ -228,8 +268,15 @@ function checkWinner() {
         playersHandTotal > TWENTY_ONE){
             winner = 'dealer';
             playSound('lossSound');
-            addWinnerGlow(winner);
+            
     } 
+    if (winner) {
+        addWinnerGlow(winner);
+        updateWinnings();
+        updateEarnings();
+        enablePlay();
+        inPlay = false;
+    }
     
     render();
 }
@@ -259,6 +306,11 @@ function flipDealerCard() {
 function render(){
     // Function to update the state variables and render to DOM
     // If the player hits, update their hand
+    if (madeBet && !inPlay || resetBet && !inPlay) {
+        updateBet();
+        return;
+    }
+    
     if (hit) {
         hit = false;
         updateCurrHandTotal();
@@ -271,6 +323,10 @@ function render(){
         disableMoves();
         updateScoreboard();
         playButtonText('play again?');
+        unlockBetWindow();
+        enableBets();
+        setBetToZero();
+        updateWallet();
     }
     
     // If the scoreboard is reset
@@ -489,12 +545,162 @@ function resetWinnerGlow() {
 }
 
 function playSound(name) {
+    // Function to play the sound effects
     player.src = sounds[name];
     player.play();
 }
 
-bgPlayer.volume = .02;
-player.volume = .03;
+function startingWallet() {
+    // Function to intiialize the starting wallet amount
+    wallet = STARTING_MONEY;
+    walletAmount.textContent = `$${wallet}`;
+}
 
-onLoad();
-createDeck();
+function handleBet(evt) {
+    // Function to handle a bet
+    if (evt.target === bet100){
+        addBet(ONE_HUNDRED);
+    } else if (evt.target === bet500) {
+        addBet(FIVE_HUNDRED);
+    } else if (evt.target === bet1000) {
+        addBet(ONE_THOUSAND);
+    } else if (evt.target === betResetButton) {
+        clearBet();
+    }
+}
+
+function addBet(amount) {
+    // Function to add a bet
+    madeBet = true;
+    if (wallet - amount >= 0){
+        unlockBetWindow();
+        bet += amount;
+        wallet -= amount;
+        updateWallet();
+        render();
+    } else if (wallet - amount < 0) {
+        betAmount.style.color = 'red';
+        if (amount === ONE_HUNDRED){
+            disableBet100();
+        } else if (amount === FIVE_HUNDRED){
+            disableBet500();
+        } else if (amount === ONE_THOUSAND){
+            disableBet1000();
+        }
+    }
+    madeBet = false;
+}
+
+function updateBet() {
+    // Function to update the text of the bet amount in the DOM
+    betAmount.textContent = `$${bet}`;
+}
+
+function clearBet() {
+    // Function to clear the bets when the clear button is pressed
+    resetBet = true;
+    updateWallet();
+    bet = 0;
+    render();
+    unlockBetWindow();
+    enableBets();
+    resetBet = false;
+}
+
+function updateWallet() {
+    // Function to update the wallet
+    if (resetBet) {
+        wallet += bet;
+    }
+    walletAmount.textContent = `$${wallet}`
+}
+
+function lockBetWindow() {
+    // Function to set the styling of the bet window to grey
+    betAmount.style.color = 'grey';
+}
+
+function unlockBetWindow() {
+    // Function to reset the styling of the bet window 
+    betAmount.style.color = 'white';
+}
+
+function updateEarnings() {
+    // Function to update the earnings
+    earnings = bet;
+    if (winner === 'tie' || winner === '') {
+        earningsAmount.style.color = 'white';
+        earningsAmount.textContent = `$0`;
+    } else if (winner === 'player') {
+        earningsAmount.style.color = 'green';
+        earningsAmount.textContent = `+$${earnings}`;
+    } else if (winner === 'dealer') {
+        earningsAmount.style.color = 'red';
+        earningsAmount.textContent = `-$${earnings}`;
+    }
+}
+
+function disableBets() {
+    // Function to disable all the bet buttons as group
+    disableBet100();
+    disableBet500();
+    disableBet1000();
+    betResetButton.disabled = true;
+}
+
+function enableBets() {
+    // Function to enable all the bet buttons as a group
+    bet100.disabled = false;
+    bet500.disabled = false;
+    bet1000.disabled = false;
+    betResetButton.disabled = false;
+}
+
+function disableBet1000() {
+    // Function to individually disable the $1000 bet
+    bet1000.disabled = true;
+}
+
+function disableBet500() {
+    // Function to individually disable the $500 bet
+    bet500.disabled = true;
+}
+
+function disableBet100() {
+    // Function to individually disable the $100 bet
+    bet100.disabled = true;
+}
+
+function setBetToZero() {
+    // Function to set the bet to zero
+    bet = 0;
+    betAmount.textContent = `$${bet}`
+}
+
+function setEarningsToZero() {
+    earnings = 0;
+}
+
+function updateWinnings() {
+    if (winner === 'player') {
+        wallet += bet * 2;
+    } else if (winner === 'tie') {
+        wallet += bet;
+    }
+}
+
+function disablePlay() {
+    playBtn.disabled = true;
+}
+
+function enablePlay() {
+    playBtn.disabled = false;
+}
+
+function disableDouble() {
+    doubleButton.disabled = true;
+}
+
+function enableDouble() {
+    doubleButton.disabled = false;
+}
